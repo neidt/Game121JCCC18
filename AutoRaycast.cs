1011fb9f37d9f97e09198fba4b4f50419f7e9f56
neidt <35543916+neidt@users.noreply.github.com>
2018-02-12 17:36:18 -0600

    stuff from 2-12
    raycasting stuff from today

---
+++ AutoRaycast.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRaycast : MonoBehaviour
+{
+    public LayerMask raycastLayers;
+    public LayerMask floorOnly;
+
+    public float rayDistance = 3.0f;
+
+    //incase we hit
+    private bool hitThisFrame = false;
+    private Vector3 rayCollisionNormal;
+    private Vector3 hitLocThisFrame = Vector3.zero;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        hitThisFrame = false;
+
+        RaycastHit hitInfo;
+        
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, rayDistance, raycastLayers.value)) 
+        {
+            print("raycast hit" + hitInfo.transform.name + " at" + hitInfo.point);
+            hitLocThisFrame = hitInfo.point;
+            rayCollisionNormal = hitInfo.normal;
+            hitThisFrame = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray intoScreen = Camera.main.ScreenPointToRay(Input.mousePosition); // hits whatever is under mouse at the time 
+            if(Physics.Raycast(intoScreen, out hitInfo, 1000, floorOnly))
+            {
+                transform.position = hitInfo.point + new Vector3(0, 1, 0);
+            }
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * rayDistance);
+    }
+}
---
+++ Follower.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public Transform playerObject;
+    public Transform follower;
+    public float followerMoveSpeed = 3.0f;
+
+    public LayerMask raycastLayers;
+    public LayerMask floorOnly;
+
+    public float rayDistance = 5.0f;
+    public float followDistance = 3f;
+
+    private bool hitPlayer = false;
+    private Vector3 rayCollisionNormal;
+    private Vector3 hitLocThisFrame = Vector3.zero;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        hitPlayer = false;
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, rayDistance, raycastLayers.value))
+        {
+            print("raycast hit " + hitInfo.transform.name + " at " + hitInfo.point);
+            hitLocThisFrame = hitInfo.point;
+            rayCollisionNormal = hitInfo.normal;
+
+            if(Vector3.Distance(follower.position, playerObject.position) > followDistance)
+            {
+                follower.transform.LookAt(playerObject);
+                follower.transform.position += follower.transform.forward * followerMoveSpeed * Time.deltaTime;
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * rayDistance);
+    }
+}
